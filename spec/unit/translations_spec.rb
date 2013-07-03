b1826286757ede691693402642522ec7125b35a9
Benjamin Michotte <bmichotte@gmail.com>
2013-07-03 08:29:30 +0200

    correct scope, correction on the locale generation

--- spec/unit/translations_spec.rb
+++ spec/unit/translations_spec.rb
@@ -8,9 +8,9 @@ Dir.glob('rails/locale/*.yml') do |locale_file|
     I18n.locale = locale_file.split('/').last.split('.').first
     translations = I18n.backend.send(:translations)
     translations.keys.each do |locale_code|
-      p I18n.t(locale_code, :scope => :locales, :separator => "\001") if I18n.t(locale_code, :scope => :locales, :separator => "\001").include?('translation missing')
-      it { I18n.t(locale_code, :scope => :locales, :separator => "\001").should_not == locale_code }
-      it { I18n.t(locale_code, :scope => :locales, :separator => "\001").include?('translation missing').should == false }
+      p I18n.t(locale_code, :scope => :languages, :separator => "\001") if I18n.t(locale_code, :scope => :languages, :separator => "\001").include?('translation missing')
+      it { I18n.t(locale_code, :scope => :languages, :separator => "\001").should_not == locale_code }
+      it { I18n.t(locale_code, :scope => :languages, :separator => "\001").include?('translation missing').should == false }
     end
   end
 end
